379221bfc8a77b0b663b5e8766eaa0e2719d0521
Selah Ben-Haim <selahb@gmail.com>
2025-11-20 12:23:47 -0600

    fit,cyc,x,el,rep,spl

--- src/cycl/p3.clj
+++ src/cycl/p3.clj
@@ -27,92 +27,54 @@
   (reduce + (map weigh cycls)))
 
 
-(defn scale-event
-  [evt segment-start segment-len period]
-  (let [start (:start evt)
-        cycle (long start)
-        frac  (- start cycle)]
-    (-> evt
-        (assoc :start (+ cycle segment-start (* frac segment-len)))
-        (update :length #(* % segment-len))
-        (update :period #(* % period)))))
-
-
 (defn scale
-  [x cycls]
-  (let [weights (map weigh cycls)
-        n       (reduce + weights)
-        offsets (reductions + 0 weights)]
-    (mapcat (fn [cycl weight offset]
-              (let [segment-start (* offset x (/ n))
-                    segment-len   (* weight x (/ n))]
-                (map
-                 (fn [evt]
-                   (let [start (:start evt)
-                         cycle (long start)
-                         frac  (- start cycle)]
-                     (-> evt
-                         ;; Retain cycle pos, offset to pos in parent, scale original offset to enclosing segment
-                         (assoc :start (+ cycle segment-start (* frac segment-len)))
-                         ;; Scale length to new enclosing segment
-                         (update :length #(* % segment-len))
-                         ;; Update periods
-                         (update :period #(* % x)))))
-                 cycl)))
-            cycls
-            weights
-            offsets)))
+  ([x cycls] (scale x cycls false))
+  ([x cycls cyclic?]
+   (let [weights (map weigh cycls)
+         n       (reduce + weights)
+         offsets (reductions + 0 weights)
+         factor  (/ x n)]
+     (mapcat (fn [cycl weight offset]
+               (map
+                (fn [evt]
+                  (let [start (:start evt)
+                        cycle (long start)
+                        frac  (- start cycle)
+                        new-cycle (if cyclic? (* n cycle) cycle)]
+                    (-> evt
+                        (assoc :start (+ new-cycle (* offset factor) (* frac weight factor)))
+                        (update :length #(* % weight factor))
+                        (update :period #(* % x)))))
+                cycl))
+             cycls weights offsets))))
 
 
 (comment
-  (scale 1 (encyclify [:a :b :c :d]))
-  (scale 1 (encyclify [:a :b (scale 1 (encyclify [:c :d]))]))
-  (scale 1 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d])) {:weight 2})]))
+  (scale 1 (encyclify [:a :b :c :d]) false)
+  (scale 1 (encyclify [:a :b (scale 1 (encyclify [:c :d]) false)]) false)
+  (scale 1 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d]) false) {:weight 2})]) false)
+  (scale 2 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d]) false) {:weight 2})]) false)
+  (scale 1/2 (encyclify [:a :b (scale 1 (encyclify [:c :d]) false)]) false))
+
+
+(comment
+  (scale 4 (encyclify [:a :b :c :d]))
+  (scale 3 (encyclify [:a :b :c :d]))
+  (scale 2 (encyclify [:a :b :c :d]))
+  (scale 3 (encyclify [:a :b (scale 2 (encyclify [:c :d]))]) true)
+  (scale 2 (encyclify [:a :b (scale 2 (encyclify [:c :d]))]) true)
+  (scale 4 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d])) {:weight 2})]))
   (scale 2 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d])) {:weight 2})]))
   (scale 1/2 (encyclify [:a :b (scale 1 (encyclify [:c :d]))])))
 
 
-(defn spread
-  [x cycls]
-  (let [weights (map weigh cycls)
-        n       (reduce + weights)
-        offsets (reductions + 0 weights)]
-    (mapcat (fn [cycl weight offset]
-              (map
-               (fn [evt]
-                 (let [start (:start evt)
-                       cycle (long start)
-                       frac (- start cycle)
-                       factor (/ x n)]
-                   (-> evt
-                       ;; Scale original cycle num by parent weight, add spread offset scaled by spreading factor, re-introduce fractional offset
-                       (assoc :start (+ (* factor offset) (* cycle n) frac)) 
-                       ;; Scale weight by spread factor ratio
-                       (update :length #(* % factor weight))
-                       ;; Scale period by x
-                       (update :period #(* % x)))))
-               cycl))
-            cycls
-            weights
-            offsets)))
-
-
-(comment
-  (spread 4 (encyclify [:a :b :c :d]))
-  (spread 3 (encyclify [:a :b :c :d]))
-  (spread 2 (encyclify [:a :b :c :d]))
-  (spread 3 (encyclify [:a :b (spread 2 (encyclify [:c :d]))]))
-  (scale 2 (encyclify [:a :b (scale 2 (encyclify [:c :d]))]))
-  (spread 4 (encyclify [:a :b (with-meta (scale 2 (encyclify [:c :d])) {:weight 2})]))
-  (scale 2 (encyclify [:a :b (with-meta (scale 1 (encyclify [:c :d])) {:weight 2})]))
-  (scale 1/2 (encyclify [:a :b (scale 1 (encyclify [:c :d]))])))
-
+;; Ops
 
 
 (defn fit
   [& pattern]
   (let [cycls (encyclify pattern)]
-    (scale 1 cycls)))
+    (scale 1 cycls false)))
 
 (comment
   (fit :a :b :c)
@@ -123,7 +85,8 @@
   [& pattern]
   (let [cycls (encyclify pattern)
         n (reduce + (map weigh cycls))]
-    (spread n cycls)))
+    (scale n cycls true)))
+
 
 (comment
   (cyc :a :b :c)
@@ -135,3 +98,65 @@
   (cyc :a (fit :b :c))
   (fit (cyc :a :b) (cyc :c (fit :d :e)) :f)
   (cyc (fit :a :b) (cyc :c (fit :d :e)) :f))
+
+
+(defn times-op
+  [n cycl]
+  (scale 1 (repeat n cycl)))
+
+
+(comment
+  (times-op 2 (fit :a))
+  (fit :a (times-op 2 (fit :b))))
+
+
+(defn x
+  [n & pattern]
+  (times-op n (apply fit pattern)))
+
+
+(defn elongate-op
+  [x cycl]
+  (with-meta cycl {:weight x}))
+
+
+(defn el
+  [x & pattern]
+  (elongate-op x (apply fit pattern)))
+
+
+(comment
+  (fit :a (spl :b :c))
+  (cyc :a (spl :b :c))
+  (fit :a (spl :b (el 2 :c)))
+  (cyc :a (spl :b (el 2 :c))))
+
+
+(comment
+  (fit :a (el 2 :b))
+  (fit :a (fit :b (el 2 :c)))
+  (cyc :a (el 2 :b))
+  (cyc :a (cyc :b (el 2 :c))))
+
+
+(defn repeat-op
+  [n cycl]
+  (elongate-op n (times-op n cycl)))
+
+
+
+(defn rep
+  [n & pattern]
+  (repeat-op n (apply fit pattern)))
+
+
+(comment
+  (fit :a (rep 2 :b))
+  (cyc :a (rep 2 :b)))
+
+
+(defn spl
+  [& pattern]
+  (let [cycls (encyclify pattern)
+        weight (reduce + (map weigh cycls))]
+    (apply el weight pattern)))
